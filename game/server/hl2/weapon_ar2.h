e5aae835c88cbf8c50996768088273cdfe6adcbb
mariovct <31172185+mariovct@users.noreply.github.com>
2021-11-03 13:18:13 -0700

    The game
    All the files pertaining to the mod.

---
+++ game/server/hl2/weapon_ar2.h
@@ -0,0 +1,86 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose:		Projectile shot from the AR2 
+//
+// $Workfile:     $
+// $Date:         $
+//
+//-----------------------------------------------------------------------------
+// $Log: $
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#ifndef	WEAPONAR2_H
+#define	WEAPONAR2_H
+
+#include "basegrenade_shared.h"
+#include "basehlcombatweapon.h"
+
+class CWeaponAR2 : public CHLSelectFireMachineGun
+{
+public:
+	DECLARE_CLASS( CWeaponAR2, CHLSelectFireMachineGun );
+
+	CWeaponAR2();
+
+	DECLARE_SERVERCLASS();
+
+	void	ItemPostFrame( void );
+	void	Precache( void );
+	
+	void	SecondaryAttack( void );
+	void	DelayedAttack( void );
+
+	const char *GetTracerType( void ) { return "AR2Tracer"; }
+
+	void	AddViewKick( void );
+
+	void	FireNPCPrimaryAttack( CBaseCombatCharacter *pOperator, bool bUseWeaponAngles );
+	void	FireNPCSecondaryAttack( CBaseCombatCharacter *pOperator, bool bUseWeaponAngles );
+	void	Operator_ForceNPCFire( CBaseCombatCharacter  *pOperator, bool bSecondary );
+	void	Operator_HandleAnimEvent( animevent_t *pEvent, CBaseCombatCharacter *pOperator );
+
+	float	GetFireRate( void ) { return 0.14f; }
+	
+	// Burst fire
+	//int		GetBurstSize( void ) { return 2; };
+	
+	// the amount of delay we want to have between bursts.
+	//float 	GetBurstCycleRate( void ){ return 0.2f; }
+	
+	// NPC
+	int		GetMinBurst( void ) { return 1; }
+	int		GetMaxBurst( void ) { return 1; }
+	float	GetMinRestTime( void ) { return 0.2f; }
+	float	GetMaxRestTime( void ) { return 0.2f; }
+
+	bool	CanHolster( void );
+	bool	Reload( void );
+
+	int		CapabilitiesGet( void ) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
+
+	Activity	GetPrimaryAttackActivity( void );
+	
+	void	DoImpactEffect( trace_t &tr, int nDamageType );
+
+	virtual const Vector& GetBulletSpread( void )
+	{
+		static Vector cone = VECTOR_CONE_1DEGREES; 
+		return cone;
+	}
+
+	const WeaponProficiencyInfo_t *GetProficiencyValues();
+
+protected:
+
+	float					m_flDelayedFire;
+	bool					m_bShotDelayed;
+	int						m_nVentPose;
+	
+	DECLARE_ACTTABLE();
+	DECLARE_DATADESC();
+};
+
+
+#endif	//WEAPONAR2_H
